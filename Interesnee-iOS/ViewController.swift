02be2a2ea639cc63d8e16a9695b8a724db0e1dd2
orbly <a.makogonenko@meta.studio>
2022-11-10 10:25:25 +0700

    Initial Commit

---
+++ Interesnee-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Interesnee-iOS
+//
+//  Created by Артем on 10.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
