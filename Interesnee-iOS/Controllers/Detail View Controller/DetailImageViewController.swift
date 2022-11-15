de618e9e841826e198cab2edc513e848048eab7e
orbly <a.makogonenko@meta.studio>
2022-11-15 13:32:48 +0700

    Refactor DetailView and WebSourceView

--- Interesnee-iOS/Controllers/Detail View Controller/DetailImageViewController.swift
+++ Interesnee-iOS/Controllers/Detail View Controller/DetailImageViewController.swift
@@ -6,29 +6,54 @@
 //
 
 import UIKit
+import Kingfisher
 
 class DetailImageViewController: UIViewController {
 
     @IBOutlet weak var nextButton: UIBarButtonItem!
     @IBOutlet weak var previousButton: UIBarButtonItem!
     @IBOutlet weak var fullImageView: UIImageView!
-
     
+    weak var viewModel: ImageDataViewModel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        updateUI()
+    }
+
+    private func updateUI() {
+        previousButton.isEnabled = viewModel.isStart()
+
+        nextButton.isEnabled = viewModel.isEnd()
+
+        fullImageView.kf.indicatorType = .activity
+        fullImageView.kf.setImage(with: viewModel.imageViewModelForSelectedCell()?.originalImage)
+    }
+
+    @IBAction func prevButtonPressed(_ sender: UIBarButtonItem) {
+        viewModel.selectedIndex -= 1
+
+        updateUI()
+    }
+
+    @IBAction func nextButtonPressed(_ sender: UIBarButtonItem) {
+        viewModel.selectedIndex += 1
+
+        updateUI()
     }
     
 
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        guard let webSourceVC = segue.destination as? WebSourceViewController else {
+            fatalError("WebSourceViewController not found!")
+        }
+
+        webSourceVC.sourceUrl = viewModel.imageViewModelForSelectedCell()?.sourceUrl
+
     }
-    */
 
 }
--- Interesnee-iOS/Controllers/WebSourceViewController/WebSourceViewController.swift
+++ Interesnee-iOS/Controllers/WebSourceViewController/WebSourceViewController.swift
@@ -11,22 +11,13 @@ import WebKit
 class WebSourceViewController: UIViewController {
 
     @IBOutlet weak var webView: WKWebView!
+    var sourceUrl: URL?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        guard let url = sourceUrl else { return }
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
